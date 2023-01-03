4284733185a6c482c0da54d73c479b3e97588e24
dawidbros1 <dawidbros1@wp.pl>
2023-01-03 13:33:12 +0100

    singleton

---
+++ singleton/src/GameEngine.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+class GameEngine
+{
+   private int $hp = 100;
+   private string $characterName = "";
+
+   private static GameEngine $instance;
+   private function __construct()
+   {
+      // private constructor prevents creating an object directly with the new operator
+   }
+
+   public static function getInstance(): GameEngine
+   {
+      return GameEngine::$instance ?? new GameEngine();
+   }
+
+   private function __clone()
+   {
+      // private cloning preventing copying of the object
+   }
+
+   private function __wakeup()
+   {
+      // private unserialization prevents the object from being recreated from the serialized string
+   }
+}
